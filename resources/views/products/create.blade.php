5a88beb319ce3d9f2a0c803022e26229bb6b372b
dannyp2293 <dannybutarbutar@gmail.com>
2025-12-02 15:33:08 +0700

    dikantor

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -10,15 +10,20 @@
 </div>
 <div class="mt-4">
     <form method="POST">
-         <div>
+         <div class="mt-4">
             <x-input-label for="nama" :value="__('Email')" />
             <x-text-input id="nama" class="block mt-1 w-full" type="text" name="nama" :value="old('nama')" required/>
             <x-input-error :messages="$errors->get('nama')" class="mt-2" />
         </div>
-         <div>
+         <div  class="mt-4">
             <x-input-label for="harga" :value="__('Harga')" />
             <x-text-input id="harga" class="block mt-1 w-full" type="text" name="harga" :value="old('harga')" required/>
-            <x-input-error :messages="$errors->get('nama')" class="mt-2" />
+            <x-input-error :messages="$errors->get('harga')" class="mt-2" />
+        </div>
+           <div  class="mt-4">
+            <x-input-label for="deskripsi" :value="__('Deskripsi')" />
+            <x-text-input id="deskripsi" class="block mt-1 w-full" type="text" name="deskripsi" :value="old('deskripsi')" required/>
+            <x-input-error :messages="$errors->get('deskripsi')" class="mt-2" />
         </div>
 
 
